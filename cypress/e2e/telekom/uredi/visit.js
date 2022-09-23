4cf2e565fc41eddf2a4f68fc438106d0c268ee5a
flepceska <flepceska@gmail.com>
2022-09-23 08:55:16 +0200

    new tests

--- cypress/e2e/telekom/uredi/visit.js
+++ cypress/e2e/telekom/uredi/visit.js
@@ -18,3 +18,7 @@ it('Check radio functionality', function() {
     cy.get('[class="paymentType"]').first().get('div > radio')
     .should('be.checked')
 })
+
+it('Occurences of Cards', function() {
+    cy.get('[class="col-lg-4 col-md-4 col-sm-6 col-xs-12 ng-scope"]').its('length').should('eq', 75)
+})
